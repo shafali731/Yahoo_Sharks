05387ecd13b3f3f4304e471d173dc262dfa86212
Bing5154 <bli7@stuy.edu>
2018-05-17 19:25:22 -0400

    RunMed add method not implemented, but compiles

--- rmed/ALHeapMax.java
+++ rmed/ALHeapMax.java
@@ -2,109 +2,117 @@ import java.util.ArrayList;
 
 public class ALHeapMax {
     //instance vars
-  private ArrayList<Integer> _heap;
+    private ArrayList<Integer> _heap;
 
-  /*****************************************************
-   * default constructor  ---  inits empty heap
-   *****************************************************/
-  public ALHeapMax()
-  {
-      _heap = new ArrayList<Integer>();
-  }
-
-
-
-  /*****************************************************
-   * toString()  ---  overrides inherited method
-   * Returns either
-   * a) a level-order traversal of the tree (simple version)
-   * b) ASCII representation of the tree (more complicated, more fun)
-   *****************************************************/
-  public String toString()
-  {
-      String retStr = "";
-      int level = 1;
-      for (int i = 0; i < _heap.size(); i++) {
-	  if(i == Math.pow(2,level) - 1) {
-	      retStr += "\n";
-	      level++;
-	  }
-	  retStr += _heap.get(i) + " ";
-      }
-      return retStr + "\n";
-  }//O(n)
+    /*****************************************************
+     * default constructor  ---  inits empty heap
+     *****************************************************/
+    public ALHeapMax()
+    {
+	_heap = new ArrayList<Integer>();
+    }
 
 
-  /*****************************************************
-   * boolean isEmpty()
-   * Returns true if no meaningful elements in heap, false otherwise
-   *****************************************************/
-  public boolean isEmpty()
-  {
-      return _heap.isEmpty();
-  }//O(1)
+
+    /*****************************************************
+     * toString()  ---  overrides inherited method
+     * Returns either
+     * a) a level-order traversal of the tree (simple version)
+     * b) ASCII representation of the tree (more complicated, more fun)
+     *****************************************************/
+    public String toString()
+    {
+	String retStr = "";
+	int level = 1;
+	for (int i = 0; i < _heap.size(); i++) {
+	    if(i == Math.pow(2,level) - 1) {
+		retStr += "\n";
+		level++;
+	    }
+	    retStr += _heap.get(i) + " ";
+	}
+	return retStr + "\n";
+    }//O(n)
 
 
-  /*****************************************************
-   * Integer peekMin()
-   * Returns min value in heap
-   * Postcondition: Heap remains unchanged.
-   *****************************************************/
-  public Integer peekMax()
-  {
-      return _heap.get(0);
-  }//O(1)
+    /*****************************************************
+     * boolean isEmpty()
+     * Returns true if no meaningful elements in heap, false otherwise
+     *****************************************************/
+    public boolean isEmpty()
+    {
+	return _heap.isEmpty();
+    }//O(1)
 
 
-  /*****************************************************
-   * add(Integer)
-   * Inserts an element in the heap
-   * Postcondition: Tree exhibits heap property.
-   * Algorithm:
-   * Insert the Integer at the end of the arrayList
-   * Keep moving up the tree (aka. swap with the parent node) if it's less
-   * Repeat the previous step until the Integer reaches the root or it is less than the parents
-   *****************************************************/
-  public void add( Integer addVal )
-  {
-      if(_heap.size() == 0) {
-	  _heap.add(addVal);
-      }
-
-      _heap.add(addVal); //insert integer at the end
-      int current = _heap.size() - 1;
-
-      //keep swapping when current is less than parent
-      while(addVal.compareTo( _heap.get((current - 1) / 2)) > 0) { //node is greater than parent
-	  swap(current , (current - 1) / 2);
-	  current = (current - 1) / 2;
-      }
+    //return size of heap, useful in runmed
+    public int getSize() {
+	return _heap.size();
+    }
 
 
-  }//O(logn)
+  
+
+    /*****************************************************
+     * Integer peekMin()
+     * Returns min value in heap
+     * Postcondition: Heap remains unchanged.
+     *****************************************************/
+    public Integer peekMax()
+    {
+	return _heap.get(0);
+    }//O(1)
 
 
-  /*****************************************************
-   * removeMin()  ---  means of removing an element from heap
-   * Removes and returns least element in heap.
-   * Postcondition: Tree maintains heap property.
-   * Algorithm:
-   * Replace the last value in the ArrayList with the first value. We wanted to swap the root with the last element because that costs minimum run time O(1) if we were to remove it
-   * Remove the last value (aka. the original root)
-   * Keep on comparing and swapping the current root with the children nodes.
+    /*****************************************************
+     * add(Integer)
+     * Inserts an element in the heap
+     * Postcondition: Tree exhibits heap property.
+     * Algorithm:
+     * Insert the Integer at the end of the arrayList
+     * Keep moving up the tree (aka. swap with the parent node) if it's less
+     * Repeat the previous step until the Integer reaches the root or it is less than the parents
+     *****************************************************/
+    public void add( Integer addVal )
+    {
+	if(_heap.size() == 0) {
+	    _heap.add(addVal);
+	}
+
+	_heap.add(addVal); //insert integer at the end
+	int current = _heap.size() - 1;
+
+	//keep swapping when current is less than parent
+	while(addVal.compareTo( _heap.get((current - 1) / 2)) > 0) { //node is greater than parent
+	    swap(current , (current - 1) / 2);
+	    current = (current - 1) / 2;
+	}
+
+
+    }//O(logn)
+
+
+    /*****************************************************
+     * removeMin()  ---  means of removing an element from heap
+     * Removes and returns least element in heap.
+     * Postcondition: Tree maintains heap property.
+     * Algorithm:
+     * Replace the last value in the ArrayList with the first value. We wanted to swap the root with the last element because that costs minimum run time O(1) if we were to remove it
+     * Remove the last value (aka. the original root)
+     * Keep on comparing and swapping the current root with the children nodes.
            If less than child, keep traversing down
            If bigger than child, algorithm ends
-   *****************************************************/
-  public Integer removeMax()
-  {
-      if ( isEmpty() )
-	  return null;
+    *****************************************************/
+    public Integer removeMax()
+    {
+	if ( isEmpty() )
+	    return null;
 
-      Integer ret = peekMax();
+	Integer ret = peekMax();
 
-      Integer temp = _heap.get( _heap.size() - 1);
-      swap( 0, _heap.size() - 1 ); 
-      _heap.remove( _heap.size() - 1);
+	Integer temp = _heap.get( _heap.size() - 1);
+	swap( 0, _heap.size() - 1 ); 
+	_heap.remove( _heap.size() - 1);
 
   	int pos = 0;
   	int maxC;
@@ -124,113 +132,113 @@ public class ALHeapMax {
   	}
   	return ret;
 
-  }//O(logn)
+    }//O(logn)
 
 
-  /*****************************************************
-   * minChildPos(int)  ---  helper fxn for removeMin()
-   * Returns index of least child, or
-   * -1 if no children, or if input pos is not in ArrayList
-   * Postcondition: Tree unchanged
-   *****************************************************/
-  private int maxChildPos( int pos )
-  {
-    int ret;
-    int lc = 2*pos + 1;
-    int rc = 2*pos + 2;
+    /*****************************************************
+     * minChildPos(int)  ---  helper fxn for removeMin()
+     * Returns index of least child, or
+     * -1 if no children, or if input pos is not in ArrayList
+     * Postcondition: Tree unchanged
+     *****************************************************/
+    private int maxChildPos( int pos )
+    {
+	int ret;
+	int lc = 2*pos + 1;
+	int rc = 2*pos + 2;
 
-    //pos not in heap or pos is at leaf
-    if ( pos >= _heap.size() || pos < 0 || lc >= _heap.size() ){
-      ret = -1;
-    }
-    //no right child
-    else if ( rc >= _heap.size() ){
-      ret = lc;
-    }
-    //no left child
-    else if (lc>= _heap.size()){
-      ret = rc;
-    }
-    //have two children, compare
-    else if ( _heap.get(rc) .compareTo(_heap.get(lc)) >  0 ){
-      ret = rc;
-    } else {
-	ret = lc;
-    }
-    return ret;
-  }//O(1)
+	//pos not in heap or pos is at leaf
+	if ( pos >= _heap.size() || pos < 0 || lc >= _heap.size() ){
+	    ret = -1;
+	}
+	//no right child
+	else if ( rc >= _heap.size() ){
+	    ret = lc;
+	}
+	//no left child
+	else if (lc>= _heap.size()){
+	    ret = rc;
+	}
+	//have two children, compare
+	else if ( _heap.get(rc) .compareTo(_heap.get(lc)) >  0 ){
+	    ret = rc;
+	} else {
+	    ret = lc;
+	}
+	return ret;
+    }//O(1)
 
 
-  //************ aux helper fxns ***************
-  private Integer maxOf( Integer a, Integer b )
-  {
-    if ( a.compareTo(b) > 0 )
+    //************ aux helper fxns ***************
+    private Integer maxOf( Integer a, Integer b )
+    {
+	if ( a.compareTo(b) > 0 )
 	    return a;
-    else
+	else
 	    return b;
-  }
+    }
 
-  //swap for an ArrayList
-  private void swap( int pos1, int pos2 )
-  {
-    _heap.set( pos1, _heap.set( pos2, _heap.get(pos1) ) );
-  }
-  //********************************************
-
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-
-      ALHeapMax pile = new ALHeapMax();
-
-      pile.add(2);
-      System.out.println(pile);
-      pile.add(4);
-      System.out.println(pile);
-      pile.add(6);
-      System.out.println(pile);
-      pile.add(8);
-      System.out.println(pile);
-      pile.add(10);
-      System.out.println(pile);
-      pile.add(1);
-      System.out.println(pile);
-      pile.add(3);
-      System.out.println(pile);
-      pile.add(5);
-      System.out.println(pile);
-      pile.add(7);
-      System.out.println(pile);
-      pile.add(9);
-      System.out.println(pile);
+    //swap for an ArrayList
+    private void swap( int pos1, int pos2 )
+    {
+	_heap.set( pos1, _heap.set( pos2, _heap.get(pos1) ) );
+    }
+    //********************************************
 
 
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      System.out.println("removing " + pile.removeMax() + "...");
-      System.out.println(pile);
-      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }//end main()
+
+    //main method for testing
+    public static void main( String[] args )
+    {
+
+	ALHeapMax pile = new ALHeapMax();
+
+	pile.add(2);
+	System.out.println(pile);
+	pile.add(4);
+	System.out.println(pile);
+	pile.add(6);
+	System.out.println(pile);
+	pile.add(8);
+	System.out.println(pile);
+	pile.add(10);
+	System.out.println(pile);
+	pile.add(1);
+	System.out.println(pile);
+	pile.add(3);
+	System.out.println(pile);
+	pile.add(5);
+	System.out.println(pile);
+	pile.add(7);
+	System.out.println(pile);
+	pile.add(9);
+	System.out.println(pile);
+
+
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMax() + "...");
+	System.out.println(pile);
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }//end main()
 
 }//end class ALHea
---
+++ rmed/RunMed.java
@@ -0,0 +1,82 @@
+/*****************************************************
+ * Class runMed
+ * calculates running median of input stream of Integers by using a min and max heap
+
+ * Algorithm for RunMed: 
+ * 1. If nextValue < root of maxheap. Add to maxheap. Else: add to min heap
+ * 2. Balance heaps if size difference >=  2: Remove from larger heap, add to smaller heap until balances
+ * 3. at any find med by 
+       a. heap size equal: med is the mean of the roots
+       b. heap size unequal: return root of max heap
+ *****************************************************/
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class RunMed {
+
+    //instance vars
+    private ALHeapMax lilVals; //lilVals is a max heap even though it has smaller values, because if think mathematically, median is the max of the smaller half of the number set
+    private ALHeapMin bigVals; //big Vals is a min heap with larger values because median is the min of the larger half of the number set
+
+    //constructor
+    public RunMed() {
+	lilVals = new ALHeapMax();
+	bigVals = new ALHeapMin();
+    }
+
+    //Returns medians in both heaps
+    //throws NoSuchElementException if heaps are empty
+    public Integer getMedian() {
+	//if min heap is empty, no element was added yet, throw exception
+	if(lilVals.isEmpty()) {
+	    throw new NoSuchElementException("No medians can be found!");
+	}
+	//if both min and max are equal in # of nodes, return average of the root
+	else if (lilVals.getSize() == bigVals.getSize()) {
+	    return((lilVals.peekMax() + bigVals.peekMin()) / 2);
+	}
+	//if max heap has more values than min heap (usually the case),
+	//then return the max of all the min values
+	else if (lilVals.getSize() > bigVals.getSize()) {
+	    return lilVals.peekMax();
+	}
+	return bigVals.peekMin();	
+    }
+
+    //adds to the appropriate heap
+    //balance the heap if size difference > 1 --> def of complete tree
+    public void add(Integer newVal) {
+	//to be implemented
+    }
+
+    public static void main(String[] args) {
+	RunMed test = new RunMed();
+	//10 addition
+	for(int i = 0; i < 10; i++) {
+	    int randVal = (int)(Math.random() * 500);
+
+	    System.out.println("\nValue being added: " + randVal);
+	    test.add(randVal);
+
+	    System.out.println("Current median: " + test.getMedian());
+	}
+
+	RunMed test1 = new RunMed();
+	//15 addition
+	for (int j = 0; j < 15; j++) {
+	    int randVal = (int)(Math.random() * 500);
+
+	    System.out.println("\nValue being added: " + randVal);
+	    test1.add(randVal);
+
+	    System.out.println("Current median: " + test1.getMedian());
+	}
+	    
+    }
+
+	
+    
+	
+}
+    
